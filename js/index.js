3383d9e9ea5a09277d985e5d2bb84e7bd7b502ef
Gui Hebling <guilherme.durso@outlook.com>
2020-05-28 12:16:41 +0200

    added google maps markers js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
+
 var map;
 var markers = [];
-
+var infoWindow;
 function initMap() {
-  var losAngeles = {
-    lat: 34.06338,
-    lng: -118.35808,
-  };
-
-  var map = new google.maps.Map(document.getElementById("map"), {
-    center: losAngeles,
-    zoom: 11,
-    mapTypeId: "roadmap",
+    var losAngeles = {
+        lat: 34.063380,
+        lng: -118.358080
+    }
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: losAngeles,
+        zoom: 8,
+        mapTypeId: "roadmap",
     styles: [
       {
         featureType: "all",
@@ -181,61 +181,63 @@ function initMap() {
         ],
       },
     ],
-  });
-
-  // We have to call the function here:
-  displayStores();
-  showStoresMarker()
+    });
+    infoWindow = new google.maps.InfoWindow();
+    displayStores()
+    showStoresMarkers()
 }
 
-// Display Stores Function:
 function displayStores() {
-  var storesHtml = "";
-  stores.forEach(function (store, index) {
-    console.log(store);
-    var address = store.addressLines;
-    var phone = store.phoneNumber;
-    storesHtml += `
-        <div class="store-container">
-            <div class="store-info-container">
-                <div class="store-address">
-                    <span>${address[0]}</span>
-                    <span>${address[1]}</span>
+    var storesHtml = "";
+    stores.forEach(function(store, index){
+        var address = store.addressLines;
+        var phone = store.phoneNumber;
+        storesHtml += `
+            <div class="store-container">
+                <div class="store-info-container">
+                    <div class="store-address">
+                        <span>${address[0]}</span>
+                        <span>${address[1]}</span>
+                    </div>
+                    <div class="store-phone-number">${phone}</div>
                 </div>
-                <div class="store-phone-number">${phone}</div>
-            </div>
-            <div class="store-number-container">
-                <div class="store-number">
-                    ${index + 1}
+                <div class="store-number-container">
+                    <div class="store-number">
+                        ${index+1}
+                    </div>
                 </div>
             </div>
-        </div>
-        `;
-  });
-  document.querySelector(".stores-list").innerHTML = storesHtml;
+        `
+    });
+    document.querySelector('.stores-list').innerHTML = storesHtml;
 }
 
-function showStoresMarker() {
-  stores.forEach(function (store, index) {
-    var latlng = new google.maps.LatLng(
-      store.coordinates.latitude,
-      store.coordinates.longitude
-    );
-    var name = store.name;
-    var address = store.addressLines[0];
-    createMarker(latlng, name, address);
-  })
+
+function showStoresMarkers() {
+    var bounds = new google.maps.LatLngBounds();
+    stores.forEach(function(store, index){
+        var latlng = new google.maps.LatLng(
+            store.coordinates.latitude,
+            store.coordinates.longitude);
+        console.log(latlng);
+        var name = store.name;
+        var address = store.addressLines[0];
+        bounds.extend(latlng);
+        createMarker(latlng, name, address);
+    })
+    map.fitBounds(bounds);
 }
+
 
 function createMarker(latlng, name, address) {
-  var html = "<b>" + name + "</b> <br/>" + address;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: latlng,
-  });
-  google.maps.event.addListener(marker, "click", function () {
-    infoWindow.setContent(html);
-    infoWindow.open(map, marker);
-  });
-  markers.push(marker);
+    var html = "<b>" + name + "</b> <br/>" + address;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: latlng
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.setContent(html);
+      infoWindow.open(map, marker);
+    });
+    markers.push(marker);
 }
